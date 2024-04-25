a9b322eca19ee0163411feb649bb65f09b200ec2
Bassel-alkhalaf <123759979+Bassel-alkhalaf@users.noreply.github.com>
2024-04-25 12:49:56 -0400

    .

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
 <div class="container my-5">
-        <h1 class="mb-5 text-center">Sign In</h1>
+        <h1 class="mb-5 text-center">Sign Up</h1>
         <form class="px-5 mx-5" action="{{ route('validate_register') }}" method="POST">
             @csrf
             
