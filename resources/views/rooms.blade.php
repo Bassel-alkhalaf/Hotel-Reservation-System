ce05e626a5f6acb177406725c81e559dfc7a3df3
Bassel-alkhalaf <123759979+Bassel-alkhalaf@users.noreply.github.com>
2024-04-26 12:01:40 -0400

    Rooms layout

--- resources/views/rooms.blade.php
+++ resources/views/rooms.blade.php
@@ -2,21 +2,73 @@
 @section('content')
 <div class="container my-5">
     <h1 class="mb-5 text-center">Rooms</h1>
+   
 
-    <div class="row">
-        @foreach ($rooms as $room)
-        <div class="col-md-4">
-            <div class="card mb-4">
-                <div class="card-body">
-                    <h5 class="card-title">{{ $room['title'] }}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Room Number: {{ $room['room_number'] }}</h6>
-                    <p class="card-text">Rate: {{ $room['rate'] }}</p>
+  @foreach ($rooms as $room)     
+    <div class="row"> 
+        <div class="card mb-3">
+            <div class="card-body row">
+                <div class="col-md-12 col-lg-6">
+                    <div id="carouselExampleCrossfade" class="carousel slide carousel-fade py-3 w-75 mx-auto" data-mdb-ride="carousel" data-mdb-carousel-init>
+                        <div class="carousel-indicators">
+                            <button
+                            type="button"
+                            data-mdb-target="#carouselExampleCrossfade"
+                            data-mdb-slide-to="0"
+                            class="active"
+                            aria-current="true"
+                            aria-label="Slide 1"
+                            ></button>
+                            <button
+                            type="button"
+                            data-mdb-target="#carouselExampleCrossfade"
+                            data-mdb-slide-to="1"
+                            aria-label="Slide 2"
+                            ></button>
+                            <button
+                            type="button"
+                            data-mdb-target="#carouselExampleCrossfade"
+                            data-mdb-slide-to="2"
+                            aria-label="Slide 3"
+                            ></button>
+                        </div>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-mdb-interval="2000">
+                            <img src="https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="A hotel room image"/>    </div>
+                            <div class="carousel-item" data-mdb-interval="2000">
+                            <img src="https://images.pexels.com/photos/237371/pexels-photo-237371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="A hotel room image"/>    </div>
+                            <div class="carousel-item" data-mdb-interval="2000">
+                            <img src="https://images.pexels.com/photos/271639/pexels-photo-271639.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" class="d-block w-100" alt="A hotel room image"/>    </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-mdb-target="#carouselExampleCrossfade" data-mdb-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-mdb-target="#carouselExampleCrossfade" data-mdb-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                        
+                    </div>
                 </div>
+                
+                    <div class="col-md-12 col-lg-6">
+                        <div class="d-flex align-items-center h-100 ps-4">
+                            <div class="w-100">
+                            <h5 class="card-title">{{ $room['title'] }}</h5>
+                            <h6 class="card-subtitle mb-2 text-muted">Room Number: {{ $room['room_number'] }}</h6>
+                            <p class="card-text">Rate: {{ $room['rate'] }}</p>
+                            <a class="btn btn-primary" href="#">Select</a>
+                            </div>
+                        </div>
+                    </div>
             </div>
         </div>
-        @endforeach
     </div>
+    @endforeach       
+    
 </div>
+
 @endsection
 
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,25 @@
             panoramic views, inviting you to unwind in style.</p>
     </div>
 </div>
-
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal Title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          Modal content goes here...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 <div id="carouselExampleCrossfade" class="carousel slide carousel-fade py-5 w-50 mx-auto" data-mdb-ride="carousel" data-mdb-carousel-init>
   <div class="carousel-indicators">
     <button
